121342f41144fbf6950400c709048a09ec608f11
Sagalakova Olesya <sagalakova_olesya@mail.ru>
2019-05-21 10:32:12 +0700

    Создали модели данных

---
+++ core/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+
+from core.models import *
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Criterion)
+class CriterionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CommissionMark)
+class CommissionMarkAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FinalMark)
+class FinalMarkAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MapMarkCriterion)
+class MapMarkCriterionAdmin(admin.ModelAdmin):
+    pass
---
+++ core/models.py
@@ -0,0 +1,102 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+MARK_CHOICES = (
+    (2, 'Неудовлетворительно'),
+    (3, 'Удовлетворительно'),
+    (4, 'Хорошо'),
+    (5, 'Отлично'),
+)
+
+
+class Profile(models.Model):
+    ROLE_CHOICES = (
+        (1, 'Председатель комиссии'),
+        (2, 'Член комиссии'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    fio = models.CharField(max_length=128, verbose_name='ФИО')
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, verbose_name='Роль')
+    degree = models.CharField(max_length=32, verbose_name='Науч. степень')
+
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
+
+    def __str__(self):
+        return self.user.username
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=16, verbose_name='Название')
+
+    class Meta:
+        verbose_name = "Группа"
+        verbose_name_plural = "Группы"
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name='Группа')
+    fio = models.CharField(max_length=128, verbose_name='ФИО')
+    diploma_name = models.CharField(max_length=128, verbose_name='Тема диплома')
+
+    class Meta:
+        verbose_name = "Студент"
+        verbose_name_plural = "Студенты"
+
+    def __str__(self):
+        return self.fio
+
+
+class Criterion(models.Model):
+    name = models.CharField(max_length=64, verbose_name='Название')
+
+    class Meta:
+        verbose_name = "Критерий"
+        verbose_name_plural = "Критерии"
+
+    def __str__(self):
+        return self.name
+
+
+class CommissionMark(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, verbose_name='Студент')
+    comission = models.ForeignKey(Profile, on_delete=models.CASCADE, verbose_name='Член комиссии')
+    mark = models.PositiveSmallIntegerField(choices=MARK_CHOICES)
+
+    class Meta:
+        verbose_name = "Оценка члена комиссии"
+        verbose_name_plural = "Оценки членов комиссии"
+
+    def __str__(self):
+        return '[{}] {} {}'.format(self.id, self.student, self.comission)
+
+
+class FinalMark(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, verbose_name='Студент')
+    chairman = models.ForeignKey(Profile, on_delete=models.CASCADE, verbose_name='Председатель')
+    mark = models.PositiveSmallIntegerField(choices=MARK_CHOICES)
+
+    class Meta:
+        verbose_name = "Оценка председателя комиссии"
+        verbose_name_plural = "Оценки председателя комиссии"
+
+    def __str__(self):
+        return '[{}] {} {}'.format(self.id, self.student, self.chairman)
+
+
+class MapMarkCriterion(models.Model):
+    commission_mark = models.ForeignKey(CommissionMark, on_delete=models.CASCADE, verbose_name='Оценка комиссии')
+    criterion = models.ForeignKey(Criterion, on_delete=models.CASCADE, verbose_name='Критерий')
+    criterion_mark = models.PositiveSmallIntegerField(choices=MARK_CHOICES)
+
+    class Meta:
+        verbose_name = "Оценка критерия"
+        verbose_name_plural = "Оценки критериев"
+
+    def __str__(self):
+        return 'Critetion {}'.format(self.id)
